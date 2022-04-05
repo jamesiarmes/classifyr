2c7509972505dc66a4f88386255573d75d3a2b34
Michael Cowden <michael.cowden@gmail.com>
2022-04-05 18:45:25 -0400

    human and export apco common incident types

--- app/models/common_incident_type.rb
+++ app/models/common_incident_type.rb
@@ -1,4 +1,16 @@
+require 'csv'
+
 class CommonIncidentType < ApplicationRecord
+  EXPORT_COLUMNS = %w[id standard version code description notes humanized_code humanized_description]
   has_many  :classifications
 
+  def self.to_csv
+    cits = all
+    CSV.generate do |csv|
+      csv << EXPORT_COLUMNS
+      cits.each do |cit|
+        csv << cit.attributes.values_at(*EXPORT_COLUMNS)
+      end
+    end
+  end
 end
