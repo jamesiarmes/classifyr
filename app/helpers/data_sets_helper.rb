e82481f1bdd3de6b7f0db284bff8d8f4c89dd130
Thibault <2647689+T-Dnzt@users.noreply.github.com>
2022-08-25 22:32:03 +0700

    Adjust data sets table (#72)

--- app/helpers/data_sets_helper.rb
+++ app/helpers/data_sets_helper.rb
@@ -4,4 +4,12 @@ module DataSetsHelper
 
     "bg-white"
   end
+
+  def location(data_set)
+    return "" unless data_set.city.present? || data_set.state.present?
+    return data_set.city if data_set.city.present? && data_set.state.blank?
+    return data_set.state if data_set.city.blank? && data_set.state.present?
+
+    "#{data_set.city}, #{data_set.state}"
+  end
 end
