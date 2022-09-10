3613c2047f8455d39ffcd6a3f1e923a1c2a2b4df
James Armes (they/them) <jarmes@codeforamerica.org>
2022-09-09 21:37:22 -0400

    Removed unnecessary forgery protection setting. (#106)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   layout :layout_by_resource
 
-  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   rescue_from NotAuthorizedError, with: :not_authorized
